8b35097bc9daffde8cd8660176d3f0e87a538178
Pearse Hutson <Pearse.Hutson@vu.com>
2019-01-06 13:58:53 -0600

    Added sslify to enforce SSL connections.

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import * as express from 'express';
+import * as sslify from 'express-sslify';
 
 const app = express();
 const port = process.env.PORT || 8081;
+const environment = process.env.NODE_ENV || 'development';
+
+// Enforce SSL connections if Prod
+if (environment === 'production') {
+    app.use(sslify.HTTPS({ trustProtoHeader: true }));
+}
 
 app.get('/', (req, res) => {
     res.json({ 'test': 'true', 'Added Prop': 'I changed!' });
